5e3328c8ea1ff9ab108d8aebab8c632f2819faed
diishqa <kkover073@gmail.com>
2025-09-09 17:53:27 +0300

    1

--- Program.cs
+++ Program.cs
@@ -2,10 +2,22 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
-
+            double chislai;
+            double chislad;
+            Console.WriteLine("Введите два числа: ");
+            chislai= Convert.ToDouble(Console.ReadLine());
+            chislad= Convert.ToDouble(Console.ReadLine());
+            double summ = chislai + chislad;
+            double razn = chislai - chislad;
+            double proi = chislai * chislad;
+            double chas = chislai / chislad;
+            Console.WriteLine("Сумма: " + summ);
+            Console.WriteLine("Разность: " + razn);
+            Console.WriteLine("Произведение: " + proi);
+            Console.WriteLine("Частное: " + chas);
+            Console.ReadLine();
         }
     }
 }
